084348e3bbdb05938aa86e4c24bc3f6e3d77a8a3
Farah Student <farah.gus@hotmail.com>
2019-03-19 15:21:25 -0400

    added .map

--- src/components/participants.js
+++ src/components/participants.js
@@ -2,15 +2,19 @@ import React from 'react';
 import STORE from './components/STORE';
 
 export default function Participants(props){
-    const inSessionArr = STORE.participants.filter(participant => participant.inSession);
+    const inSessionArr = STORE.participants.filter(participant => participant.inSession === true);
+    const outOfSessionArr = STORE.participants.filter(participant => participant.inSession === false);
 
-    return (  
-      <div>
-          <h3>{props.name}</h3>
+    const inSessionHTML = inSessionArr.map(user => {
+      return (  
+        <div className="participant-box">
+          <img src={props.avatar} className="user-logo" alt="logo" />  
+          <span className="participant-name">{props.name}</span>
           <p>{props.inSession}</p>
-          <img src={props.avatar} className="App-logo" alt="logo" />  
-      </div>
-       
-    )
+            
+      </div> 
+      )
+    })
+    return inSessionHTML;
 }
       
--- src/components/stage.js
+++ src/components/stage.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import STORE from './components/STORE';
+
 
 export default function Stage(props){
-    return (
-        <section> {/* section for entire stage */}
-            <div> {/* div for each person */}
-                <div>{/* div for person name*/}
+    const userOnStage = STORE.participants.filter(participant => participant.onStage === true);
+
+    const onStageHTML = userOnStage.map(user => {
+        return (
+        <section className="main-stage"> {/* section for entire stage */}
+            <div className="participant-stage"> {/* div for each person */}
+                <div className="participant-name">{/* div for person name*/}
                     <p>{props.name}</p>
                 </div> 
                 <img src={props.avatar} className="App-logo" alt="logo" />  
             </div>
         </section>
-    )
+        )
+    })
+    return onStageHTML;
 }
